4cba225f85a6333f85eb441529e4832de6ea4fe4
vuthaphai <vuthaphai@gmail.com>
2023-12-07 20:31:09 +0700

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:  ["./*.html"],
+  content: ['./*.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
-      spacing: {
-        128:'32rem',
-      }
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+      
     },
   },
   plugins: [],
 }
+
 
